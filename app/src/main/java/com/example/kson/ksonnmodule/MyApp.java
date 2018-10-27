dbab6cecdcf0c3b71686be3ccc85a41c31e892f4
徐庆臣 <19655910@qq.com>
2018-10-27 16:43:35 +0800

    Initial commit

---
+++ app/src/main/java/com/example/kson/ksonnmodule/MyApp.java
@@ -0,0 +1,20 @@
+package com.example.kson.ksonnmodule;
+
+import android.app.Application;
+
+
+/**
+ * Author:kson
+ * E-mail:19655910@qq.com
+ * Time:2018/10/27
+ * Description:
+ */
+public class MyApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+//        ARouter.init(this);
+
+    }
+}
---
+++ module-zxing/src/main/module/java/com/example/kson/module_zxing/www.java
@@ -0,0 +1,10 @@
+package com.example.kson.module_zxing;
+
+/**
+ * Author:kson
+ * E-mail:19655910@qq.com
+ * Time:2018/10/27
+ * Description:
+ */
+public class www {
+}
